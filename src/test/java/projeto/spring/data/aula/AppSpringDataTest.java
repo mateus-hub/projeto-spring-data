e5981fcb6ae8c934b5fb5e4c64c41365ee5fef98
mateus-hub <theusskt2018@gmail.com>
2024-05-14 17:43:49 -0300

    realizando cadastro no banco de dados

--- src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
@@ -17,6 +18,16 @@ public class AppSpringDataTest {
 	
 	@Test
 	public void testeInsert() {
-		System.out.println("Iniciou spring com sucesso");
+		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+		usuarioSpringData.setEmail("javaavancado@javaavancado.com");
+		usuarioSpringData.setIdade(31);
+		usuarioSpringData.setLogin("teste 123");
+		usuarioSpringData.setSenha("123");
+		usuarioSpringData.setNome("Marcos");
+		
+		interfaceSpringDataUser.save(usuarioSpringData);
+		
+		System.out.println("Usuario cadastrado -> " + interfaceSpringDataUser.count());
+		
 	}
 }
